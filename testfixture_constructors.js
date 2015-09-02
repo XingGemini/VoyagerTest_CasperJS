383bc7e744e70247427fadb47349f6c3d4ae5308
xuxingbgi <xuxing@genomics.cn>
2015-09-02 16:12:33 -0700

    Add new user test cases

--- testfixture_constructors.js
+++ testfixture_constructors.js
@@ -1,11 +1,27 @@
 // Institution.
-var DATAOPTION_MAP = {}
+var DATAOPTION_MAP = {};
 DATAOPTION_MAP['Individual'] = 'individual';
 DATAOPTION_MAP['Clinical'] = 'clinical-lab';
 DATAOPTION_MAP['Research'] = 'research';
 DATAOPTION_MAP['Professional Society']='professional-society';
 DATAOPTION_MAP['LSDB'] = 'lsdb';
 DATAOPTION_MAP['Genomic DB'] = 'genomic-database';
+
+
+var PRVILEGE_MAP = {};
+PRVILEGE_MAP['MANAGE_INSTITUTION']="Manage Institution";
+PRVILEGE_MAP['MANAGE_INSTANCE']="Instance Admin";
+PRVILEGE_MAP['MANAGE_USER']="Manage Users";
+PRVILEGE_MAP['MANAGE_GROUP']="Manage Groups";
+PRVILEGE_MAP['MANAGE_FILTER']="Manage Filters";
+PRVILEGE_MAP['MANAGE_TEST']="Manage Tests";
+PRVILEGE_MAP['MANAGE_CASE']="Manage Cases";
+PRVILEGE_MAP['MANAGE_CONTACTS']="Manage Contacts";
+PRVILEGE_MAP['MANAGE_KNOWLEDGE_BASE']="Manage Knowledge Base";
+PRVILEGE_MAP['MANAGE_REPORT_TEMPLATE']="Manage Report Templates";
+PRVILEGE_MAP['MANAGE_IMPORT']="Manage Imports";
+PRVILEGE_MAP['MANAGE_DASHBOARD']="Access Dashboard";
+
 
 function institution (name) {
 	this.name = name;
@@ -36,3 +52,13 @@ function bucket (name, bucketPath) {
 	this.bucketPath = bucketPath;
 	this.region = "US East (Northern Virginia) Region";
 } 
+
+// User
+function usr (firstName, lastName, email, password, privilege) {
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.email = email;
+	this.password = password;
+	this.privilege = privilege;
+}
+
